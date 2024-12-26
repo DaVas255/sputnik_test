154ded7d1e43a839ccaa2a3c5ab168a4738784fb
Danil Vasilchikov <DaVas255@gmail.com>
2024-12-26 23:57:32 +0700

    update request fetch todos

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -1,5 +1,16 @@
+import { ToDo } from "./types";
+
 const API_URL = import.meta.env.VITE_API_URL as string;
 
-export function fetchTodos() {
-  return fetch(API_URL).then((res) => res.json());
+export async function fetchTodos() {
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) throw new Error(response.statusText);
+
+    const data = await response.json();
+    return data as ToDo[];
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
