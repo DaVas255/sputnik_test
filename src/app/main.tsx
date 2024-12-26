f0eab868bc4dab253fc162b150f9b611a5fa80ba
Danil Vasilchikov <DaVas255@gmail.com>
2024-12-27 00:08:36 +0700

    feat redux

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-import './index.css'
-import { TodoListPage } from '@/pages/ToDoListPage'
+import './index.css';
+import { TodoListPage } from '@/pages/ToDoListPage';
+import { store } from '@/app/store/store';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <TodoListPage />
-  </StrictMode>,
-)
+	<Provider store={store}>
+		<StrictMode>
+			<TodoListPage />
+		</StrictMode>
+	</Provider>
+);
